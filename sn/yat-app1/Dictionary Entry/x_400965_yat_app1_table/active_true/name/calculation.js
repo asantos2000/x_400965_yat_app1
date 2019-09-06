6a3360141ad740c71c9de74b42b762f858935d7b
System Administrator <admin@example.com>
2019-09-06 17:37:08 +0000

    Changes by 'System Administrator' on 'yat-app1 – 1.0.0' -- Build #1

---
+++ sn/yat-app1/Dictionary Entry/x_400965_yat_app1_table/active_true/name/calculation.js
@@ -0,0 +1,13 @@
+/* 
+ * Application : yat-app1
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-09-06 15:03:43
+ * Created By  : admin
+ * Updated On  : 2019-09-06 15:17:41
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=9a3ec86cdbbf33006a6ad001cf9619c2
+ */
+(function calculatedFieldValue(current) {
+	// Add your code here
+	return current.name.toUpperCase();  // return the calculated value
+})(current);
